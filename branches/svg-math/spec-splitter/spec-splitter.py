84431096fef104cc6bf22c66969691fa4494baf0
jgraham.cantab <jgraham.cantab@fac1fef6-d828-0410-b4ea-9384b9858573>
2007-11-09 22:05:49 +0000

    Make svg-math branch

---
+++ branches/svg-math/spec-splitter/spec-splitter.py
@@ -0,0 +1,222 @@
+import sys
+import html5lib
+
+print "HTML5 Spec Splitter";
+
+print "Parsing...";
+
+# parse document as HTML5
+parser = html5lib.html5parser.HTMLParser(tree=html5lib.treebuilders.dom.TreeBuilder)
+doc = parser.parse(open(sys.argv[1]), encoding='utf-8')
+
+print "Splitting...";
+
+# Extract the body from the source document
+original_body = doc.getElementsByTagName('body')[0]
+
+# Create an empty body, for the page content to be added into later
+default_body = doc.createElement('body')
+default_body.setAttribute('class', original_body.getAttribute('class'))
+original_body.parentNode.replaceChild(default_body, original_body)
+doc.removeChild(doc.firstChild) # remove the doctype, else doc.cloneNode dies
+
+# Extract the header, so we can reuse it in every page
+head = original_body.getElementsByTagName('div')[0]
+
+# Make a stripped-down version of it
+short_head = head.cloneNode(True)
+short_head.childNodes = short_head.childNodes[:6]
+
+# Get the annotation script too
+script = original_body.getElementsByTagName('script')[-1]
+script.parentNode.removeChild(script)
+
+# Stuff for fixing up references:
+
+# Finds all the ids and remembers which page they were on
+id_pages = {}
+def extract_ids(page, node):
+        if node.nodeType == node.ELEMENT_NODE and node.hasAttribute('id'):
+                id_pages[node.getAttribute('id')] = page
+        for n in node.childNodes:
+                extract_ids(page, n)
+
+# Updates all the href="#id" to point to page#id
+def fix_refs(page, node):
+        if node.nodeType == node.ELEMENT_NODE and node.hasAttribute('href') and node.getAttribute('href')[0] == '#':
+		id = node.getAttribute('href')[1:]
+		if id in id_pages:
+			if id_pages[id] != page: # only do non-local links
+				node.setAttribute('href', '%s.html#%s' % (id_pages[id], id))
+                else:
+			print "warning: can't find target for #%s" % id
+        for n in node.childNodes:
+		fix_refs(page, n)
+
+pages = [] # for saving all the output, so fix_refs can be called in a second pass
+
+
+# Contents/intro page:
+
+page = doc.cloneNode(True)
+page_body = page.getElementsByTagName('body')[0]
+
+# Keep moving stuff from the front of the source document into this
+# page, until we find the first heading that isn't class="no-toc"
+while not (original_body.firstChild.nodeName == 'h2'
+           and 'no-toc' not in original_body.firstChild.getAttribute('class').split(' ')):
+	extract_ids('index', original_body.firstChild)
+	page_body.appendChild(original_body.firstChild)
+page_body.appendChild(script.cloneNode(True))
+pages.append( ('index', page, 'Front cover') )
+
+
+# Section/subsection pages:
+
+def getNodeText(node):
+	if node.nodeType == node.TEXT_NODE:
+		return node.nodeValue
+	return ''.join(getNodeText(n) for n in node.childNodes)
+
+split_exceptions = ['video', 'the-canvas', 'the-command', 'tokenisation', 'tree-construction']
+
+while original_body.firstChild:
+	# Handle the heading for this section
+	heading = original_body.firstChild
+	assert(heading.nodeName[0] == 'h') # guaranteed by the other loops
+	title = getNodeText(heading)
+	name = 'section-%s' % heading.getAttribute('id')
+	print name
+
+	page = doc.cloneNode(True)
+	page_body = page.getElementsByTagName('body')[0]
+
+	# Add the header
+	page_body.appendChild(short_head.cloneNode(True))
+
+	# Add the page heading
+	page_body.appendChild(heading)
+	extract_ids(name, heading)
+
+	# Keep moving stuff from the source, until we reach the end of the
+	# document or find a header to split on
+	while original_body.firstChild and not (
+			original_body.firstChild.nodeType == doc.ELEMENT_NODE and (
+				original_body.firstChild.nodeName == 'h2' or
+				original_body.firstChild.nodeName == 'h3' or
+				original_body.firstChild.getAttribute('id') in split_exceptions
+			)):
+		extract_ids(name, original_body.firstChild)
+		page_body.appendChild(original_body.firstChild)
+
+	# Add the annotation script
+	page_body.appendChild(script.cloneNode(True))
+
+	pages.append( (name, page, title) )
+
+# Fix the links, and add some navigation:
+
+for i in range(len(pages)):
+	name, doc, title = pages[i]
+
+	fix_refs(name, doc)
+
+	if name == 'index': continue # don't add nav links to the TOC page
+
+	head = doc.getElementsByTagName('head')[0]
+
+	nav = page.createElement('nav')
+	nav.setAttribute('id', 'nav-bar')
+
+	if i != 0:
+		href = '%s.html#nav-bar' % pages[i-1][0]
+		title = pages[i-1][2]
+		a = page.createElement('a')
+		a.setAttribute('href', href)
+		a.appendChild(page.createTextNode('< %s' % title))
+		nav.appendChild(a)
+		nav.appendChild(page.createTextNode(u' \u2013 '))
+		link = page.createElement('link')
+		link.setAttribute('href', href)
+		link.setAttribute('title', title)
+		link.setAttribute('rel', 'prev')
+		head.appendChild(link)
+
+	a = page.createElement('a')
+	a.setAttribute('href', 'index.html#contents')
+	a.appendChild(page.createTextNode('Table of contents'))
+	nav.appendChild(a)
+	link = page.createElement('link')
+	link.setAttribute('href', 'index.html#contents')
+	link.setAttribute('title', 'Table of contents')
+	link.setAttribute('rel', 'index')
+	head.appendChild(link)
+
+	if i != len(pages)-1:
+		href = '%s.html#nav-bar' % pages[i+1][0]
+		title = pages[i+1][2]
+		a = page.createElement('a')
+		a.setAttribute('href', href)
+		a.appendChild(page.createTextNode('%s >' % title))
+		nav.appendChild(page.createTextNode(u' \u2013 '))
+		nav.appendChild(a)
+		link = page.createElement('link')
+		link.setAttribute('href', href)
+		link.setAttribute('title', title)
+		link.setAttribute('rel', 'next')
+		head.appendChild(link)
+
+	body = doc.getElementsByTagName('body')[0]
+	body.insertBefore(nav, body.childNodes[1]) # after the header
+
+
+def html5Serializer(element):
+	element.normalize()
+	rv = []
+	specialtext = ['style', 'script', 'xmp', 'iframe', 'noembed', 'noframes', 'noscript']
+	empty = ['area', 'base', 'basefont', 'bgsound', 'br', 'col', 'embed', 'frame',
+			 'hr', 'img', 'input', 'link', 'meta', 'param', 'spacer', 'wbr']
+
+	def escapeHTML(str):
+		return str.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;').replace('"', '&quot;')
+
+	def serializeElement(element):
+		if element.nodeType == element.DOCUMENT_TYPE_NODE:
+			rv.append("<!DOCTYPE %s>" % element.name)
+		elif element.nodeType == element.DOCUMENT_NODE:
+			for child in element.childNodes:
+				serializeElement(child)
+		elif element.nodeType == element.COMMENT_NODE:
+			rv.append("<!--%s-->" % element.nodeValue)
+		elif element.nodeType == element.TEXT_NODE:
+			unescaped = False
+			n = element.parentNode
+			while n is not None:
+				if n.nodeName in specialtext:
+					unescaped = True
+					break
+				n = n.parentNode
+			if unescaped:
+				rv.append(element.nodeValue)
+			else:
+				rv.append(escapeHTML(element.nodeValue))
+		else:
+			rv.append("<%s" % element.nodeName)
+			if element.hasAttributes():
+				for name, value in element.attributes.items():
+					rv.append(' %s="%s"' % (name, escapeHTML(value)))
+			rv.append(">")
+			if element.nodeName not in empty:
+				for child in element.childNodes:
+					serializeElement(child)
+				rv.append("</%s>" % element.nodeName)
+	serializeElement(element)
+	return '<!DOCTYPE HTML>\n' + ''.join(rv)
+
+print "Outputting...";
+
+# Output all the pages
+for name, doc, title in pages:
+	open('%s/%s.html' % (sys.argv[2], name), 'w').write(html5Serializer(doc).encode('utf-8'))
+
+print "Done.";
---
+++ branches/svg-math/tables/web/cgi-bin/table_inspector.py
@@ -0,0 +1,228 @@
+#!/usr/bin/env python
+import sys
+import urlparse
+import urllib2
+import cgi
+import cgitb
+cgitb.enable()
+import itertools
+
+import html5lib
+from html5lib import treewalkers
+import lxml.etree
+import genshi
+from genshi.template import MarkupTemplate
+from genshi.core import QName, Attrs
+from genshi.core import START, END, TEXT, COMMENT, DOCTYPE
+    
+import tableparser
+import headers
+from headers import html4, html5, experimental, smartcolspan, smartrowspan
+
+template_filename = "table_output.html"
+
+debug=True
+
+#From the html5lib test suite; need to put this somewhere sensible
+def GenshiAdapter(treewalker, tree):
+    text = None
+    for token in treewalker(tree):
+        token_type = token["type"]
+        if token_type in ("Characters", "SpaceCharacters"):
+            if text is None:
+                text = token["data"]
+            else:
+                text += token["data"]
+        elif text is not None:
+            assert type(text) in (unicode, None)
+            yield TEXT, text, (None, -1, -1)
+            text = None
+
+        if token_type in ("StartTag", "EmptyTag"):
+            yield (START,
+                   (QName(token["name"]),
+                    Attrs([(QName(attr),value) for attr,value in token["data"]])),
+                   (None, -1, -1))
+            if token_type == "EmptyTag":
+                token_type = "EndTag"
+
+        if token_type == "EndTag":
+            yield END, QName(token["name"]), (None, -1, -1)
+
+        elif token_type == "Comment":
+            yield COMMENT, token["data"], (None, -1, -1)
+
+        elif token_type == "Doctype":
+            yield DOCTYPE, (token["name"], None, None), (None, -1, -1)
+
+        else:
+            pass # FIXME: What to do?
+
+    if text is not None:
+        yield TEXT, text, (None, -1, -1)
+
+def parseDocument(document):
+    if hasattr(document, "info"):
+        charset = document.info().getparam("charset")
+    else:
+        charset="utf-8"
+    parser = html5lib.HTMLParser(tree=html5lib.treebuilders.getTreeBuilder("etree", lxml.etree))
+    tree = parser.parse(document, encoding=charset)
+    return tree
+
+def extractTables(data):
+    tree = parseDocument(data)
+    return tree.xpath("//table")
+
+def copySubtree(in_root, out_root):
+    out_root.text = in_root.text
+    out_root.tail = in_root.tail
+    for element in in_root.iterchildren():
+        if isinstance(element.tag, basestring):
+            new_element = lxml.etree.SubElement(out_root, element.tag, attrib=element.attrib)
+            copySubtree(element, new_element)
+        elif element.tag is lxml.etree.Comment:
+            new_element = lxml.etree.Comment(element.text)
+            new_element.tail = element.tail
+            out_root.append(new_element)
+
+
+def addStringListAttr(attr_name, element, value):
+    """Add a class name to an element""";
+    if attr_name in element.attrib:
+        element.attrib[attr_name] += " " + value
+    else:
+        element.attrib[attr_name] = value
+
+def addClass(element, class_name):
+    return addStringListAttr("class", element, class_name)
+
+def annotateTable(table_tree, table, heading_parser, table_id):
+    """Take an input table element and return the table with each cell annotated
+    with its heading inforamtion"""
+    all_headings = heading_parser.matchAll(table)
+    out_tree = lxml.etree.Element("table")
+    copySubtree(table_tree, out_tree)
+    
+    #Make dict mapping input tree elements to output elements tuples
+    element_map = {}
+    for in_element, out_element in itertools.izip(table_tree.iterdescendants(),
+                                                  out_tree.iterdescendants()):
+        if in_element.tag in ("td","th"):
+            cell = table.getCellByElement(in_element)
+            if not cell:
+                continue
+        element_map[in_element] = out_element
+    
+    #Add classnames to all headings
+    heading_classnames = {}
+    heading_ids = {}
+    heading_counter = itertools.count()
+    
+    for in_element, out_element in element_map.iteritems():
+        cell = table.getCellByElement(in_element)
+        if not cell:
+            continue
+        headings = all_headings[cell]
+        if headings:
+            container = lxml.etree.Element("div", attrib={"class":"__tableparser_heading_container"})
+            #Add a paragraph to the cell identifying the headings
+            title = lxml.etree.SubElement(container, "p", attrib={"class":"__tableparser_heading_title"})
+            title.text = "Headings:"
+            heading_list = lxml.etree.SubElement(container, "ul", attrib={"class":"__tableparser_heading_list"})
+            for heading in headings:
+                #Check if the heading is one we have encountered before.
+                #If not, add a unique identifier for it
+                if heading not in heading_classnames:
+                    i= heading_counter.next()
+                    class_name = "__tableparser_heading_classname_%s_%i"%(table_id, i)
+                    id = "__tableparser_heading_id_%s_%i"%(table_id, i)
+                    heading_out_element = element_map[heading.element]
+                    addClass(heading_out_element, class_name)
+                    heading_out_element.attrib['id'] = id
+                    heading_classnames[heading] = class_name
+                    heading_ids[heading] = id
+                #For each heading, copy the subelements of the heading to the cell
+                heading_data = lxml.etree.Element("li", attrib={"class":"__tableparser_heading_listitem"})
+                copySubtree(heading.element, heading_data)
+                heading_list.append(heading_data)
+                #Add a class to the cell to match the heading
+                addClass(out_element, heading_classnames[heading]+"_ref")
+                addStringListAttr("headers", out_element, heading_ids[heading])
+            out_element.insert(0, container)
+            container.tail = out_element.text
+            out_element.text=""
+        
+    return out_tree
+
+def tableToStream(table_tree, heading_parser, table_id):
+    """Take a tree for a table and return a (table,stream) pair where
+    table is a tableparser.Table and stream is a genshi stream"""
+    table = tableparser.TableParser().parse(table_tree)
+    annotated_tree = annotateTable(table_tree, table, heading_parser, table_id)
+    tw = treewalkers.getTreeWalker("etree", lxml.etree)
+    return (table, GenshiAdapter(tw, annotated_tree))
+
+def main():
+    print "content-type:text/html;charset=utf-8\n\n"
+    form = cgi.FieldStorage()
+    uri = True and form.getfirst("uri") or ""
+    if not uri:
+        source = form.getfirst("source")
+    else:
+        try:
+            source = urllib2.urlopen(uri)
+        except urllib2.URLError:
+            error("CANT_LOAD", uri)
+
+    if not source and urlparse.urlsplit(uri)[0] not in ("http", "https"):
+        error("INVALID_URI", uri)
+    
+    tables = extractTables(source)
+        
+    try:
+        use_algorithm = form.getfirst("algorithm")
+        use_scope = bool(form.getfirst("scope") == "1")
+        use_headers = bool(form.getfirst("headers") == "1")
+        
+        if use_algorithm == "html4":
+            use_scope = bool(form.getfirst("scope") == "1")
+            use_headers = bool(form.getfirst("headers") == "1")
+            heading_parser = html4.HeadingMatcher(use_scope, use_headers)
+        elif use_algorithm == "html5":
+            heading_parser = html5.HeadingMatcher()
+        elif use_algorithm == "experimental":
+            use_scope = bool(form.getfirst("scope") == "1")
+            use_headers = bool(form.getfirst("headers") == "1")
+            use_td_b_headings = bool(form.getfirst("b_headings") == "1")
+            use_td_strong_headings = bool(form.getfirst("strong_headings") == "1")
+            heading_parser = experimental.HeadingMatcher(use_scope, use_headers,
+                                                         use_td_strong_headings,
+                                                         use_td_b_headings)
+        elif use_algorithm == "smartcolspan":
+            no_headings_if_spans_data_col = bool(form.getfirst("no_headings_if_spans_data_col") == "1")
+            heading_parser = smartcolspan.HeadingMatcher(no_headings_if_spans_data_col)
+        elif use_algorithm == "smartrowspan":
+            heading_parser = smartrowspan.HeadingMatcher()
+        else:
+            raise
+    
+        data = [tableToStream(table, heading_parser, str(i)) for i, table in enumerate(tables)]
+        out_template = MarkupTemplate(open(template_filename))
+        stream = out_template.generate(data=data)
+        print stream.render('html', doctype=("html", "", ""))
+    except:
+        if debug:
+            raise
+        else:
+            error("", uri)
+    
+
+def error(error_type, input_uri):
+    out_template = MarkupTemplate(open("error.xml"))
+    stream = out_template.generate(uri=input_uri, errorType=error_type)
+    print stream.render('html', doctype=("html", "", ""))
+    sys.exit(1)
+
+if __name__ == "__main__":
+    main()
